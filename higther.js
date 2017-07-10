62696e62cab98db8e5ac1cf0e6798ddaf8adf198
0o-roki-o0 <anime.roki@gmail.com>
2017-07-10 15:02:01 +0900

    引数を渡すように変更

--- higther.js
+++ higther.js
@@ -1,12 +1,13 @@
-function myHigherFunction (func) {
+//高階関数
+function myHigherFunction () {
 console.log('myFunction');
 return outsideFunction
 }
-
-function outsideFunction () {
-console.log('outsideFunction')
+//普通の関数
+function outsideFunction (value) {
+console.log("value :" + value)
 }
 
-let returnValue = myHigherFunction();
-returnValue();
-
+// let returnValue = myHigherFunction();
+// returnValue('引数だよ');
+myHigherFunction()("aa")
